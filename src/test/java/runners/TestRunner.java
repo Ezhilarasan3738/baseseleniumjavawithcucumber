87abec7904fae1a52a5c8bfb70b9ceb17d21987b
Ezhilarasan3738 <ezhil.arasan62@gmail.com>
2025-11-01 15:45:06 +0530

    Added report line

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 		features = "src/test/resources/features/FTK/USRMR2025/2502/google.feature",
 		glue = {"ftkstepdefinition"},
 		tags = "@current",
-		plugin= {"pretty","html:target/cucumber.html"}
+		plugin = {"pretty", "html:target/cucumber-reports/html-report.html", "json:target/cucumber-reports/cucumber.json"}
 )
 public class TestRunner {
 }
